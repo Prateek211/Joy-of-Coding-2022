9995a40eb05685bcfb429cb15a7552ff0644bdbf
Prateek <92153146+Prateek211@users.noreply.github.com>
2022-05-26 00:41:08 +0530

    Add files via upload
    Separating vowels and consonants from a sentence

---
+++ Vowel.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    char a[100],v[100],c[100],i=0,j=0,k=0;
+    printf("Enter the sentence:\n");
+    gets(a);
+    while(a[i]!='\0')
+    {
+        if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u'||a[i]=='A'||a[i]=='E'||a[i]=='I'||a[i]=='O'||a[i]=='U')
+        {
+            v[j]=a[i];
+            j++;
+        }
+        else
+        {
+            c[k]=a[i];
+            k++;
+        }
+        i++;
+    }
+    v[j]='\0';
+    c[k]='\0';
+    printf("Vowels:%s\n", v);
+    printf("Consonants:%s", c);
+    return 0;
+}
